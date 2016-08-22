c2741d10933a73cd94496ecd379572d60b954c90
W. Isaac Leavitt <iceman.ike@gmail.com>
2016-08-22 16:02:45 -0600

    Delete config.js

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  connectionString: "postgres://postgres:unnamed1@localhost/levt_app",
-  sessionSecret: 'jasf832SADhfoaghO832SNLFb33fbSLJ9f',
-  clientID: '1052577242528-aou49at1l3e5b9pqqv5al73vitea1n9c.apps.googleusercontent.com',
-  clientSecret: 'fRTuPwL5qwoEMm0IsGF-33Mx',
-};
